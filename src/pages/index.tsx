ccea4dc39bd1d0a6f30fa618aa92c3bb0b29bd65
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2021-12-22 17:56:16 +0900

    Setup tailwind.

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <h1 className="text-3xl to-red-400 font-bold underline">Hello world!</h1>
+  );
+};
+
+export default Home;
